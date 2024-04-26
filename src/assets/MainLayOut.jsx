50627f0457cb3a7baf11650b387dab58e7f41ceb
ziamnoun <ziamziam05@gmail.com>
2024-04-26 22:18:34 +0600

    Basic set up done

---
+++ src/assets/MainLayOut.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayOut = () => {
+    return (
+        <div className='text-green'>
+            hello<span className='text-red text-5xl'>worl</span>
+        </div>
+    );
+};
+
+export default MainLayOut;
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import MainLayOut from "./assets/MainLayOut";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayOut></MainLayOut>
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
