e88d41a5cbe10726fbccc44360e521cdd44e7196
ziamnoun <ziamziam05@gmail.com>
2024-04-27 23:29:58 +0600

    send to server

--- src/assets/AddCraftItem/AddCraftItem.jsx
+++ src/assets/AddCraftItem/AddCraftItem.jsx
@@ -1,11 +1,65 @@
 import React from 'react';
 
 const AddCraftItem = () => {
+  const handleAddCraft=event=>{
+    event.preventDefault()
+    const form = event.target;
+    const imageURL = form.image.value;
+    const itemName = form.itemName.value;
+    const subcategoryName = form.subcategoryName.value;
+    const shortDescription = form.shortDescription.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const customization = form.customization.value;
+    const processingTime = form.processingTime.value;
+    const stockStatus = form.stockStatus.value;
+    const userEmail = form.userEmail.value;
+    const userName = form.userName.value;
+
+
+
+    const craftItemData = {
+      imageURL,
+      itemName,
+      subcategoryName,
+      shortDescription,
+      price,
+      rating,
+      customization,
+      processingTime,
+      stockStatus,
+      userEmail,
+      userName,
+      
+    };
+    console.log(craftItemData)
+    fetch('http://localhost:5000/data',{
+      method:'POST',
+      headers:{
+        'content-type':'application/json'
+      },
+      body: JSON.stringify(craftItemData)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data)
+    })
+
+
+  };
+
+ 
+
+
+
+
+
+
     return (
         <div className="min-h-screen flex items-center justify-center mt-10">
       <div className="max-w-md w-full px-6 py-8 bg-black text-white rounded-lg shadow-lg">
         <h2 className="text-2xl font-semibold  mb-4">Add Craft Item</h2>
-        <form>
+        <form onSubmit={handleAddCraft}>
           <div className="mb-4">
             <label htmlFor="image" className="block  mb-2">Image URL</label>
             <input type="text" id="image" name="image" className="w-full px-3 py-2 leading-tight text-gray-700 bg-gray-200 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" />
