146e7c5aa785a4b61aeac39e77c5a379ec97d9df
ziamnoun <ziamziam05@gmail.com>
2024-04-28 00:13:58 +0600

    card design done

--- src/assets/Home/Home.jsx
+++ src/assets/Home/Home.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+  const AllCraftItem=useLoaderData();
+
+
+
     return (
         <div>
           <Banner></Banner> 
-          <div className="div mt-5">
-          <div className="card w-96 glass text-white">
-  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">Life Hack</h2>
-    <h3>Subcategory: Car Parking</h3>
-    <p><strong>Price:</strong> $9.99</p>
-    <p><strong>Rating:</strong> 4.5/5</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary bg-red-600">View Details</button>
-    </div>
-  </div>
-</div>
+          <div className="div mt-5 grid md:grid-cols-3 grid-cols-1 gap-3 mx-auto md:w-[80%]">
+         {
+          AllCraftItem.map(item=>(
+            <div className="card w-96 glass bg-black text-white">
+            <figure><img src={item.imageURL} alt="car!"/></figure>
+            <div className="card-body">
+              <h2 className="card-title">{item.itemName}</h2>
+              <h3>Subcategory: {item.subcategoryName}</h3>
+              <p><strong>Price:</strong> {item.price}</p>
+              <p><strong>Rating:</strong>{item.rating}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary bg-red-600">View Details</button>
+              </div>
+            </div>
+          </div>
+          ))
+         }
 
         </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,8 @@ const router = createBrowserRouter([
     element: <MainLayOut></MainLayOut>,
     children:[
       { path: "/",
-      element: <Home></Home>
+      element: <Home></Home>,
+      loader:()=>fetch('http://localhost:5000/data')
     },
     { path: "/AddCraftItem",
     element: <AddCraftItem></AddCraftItem>
